0b03db8c17a13682a46c0bb60a3198aaa7c91bd5
Jenya Poznyak <jendy.light@gmail.com>
2020-04-10 20:26:15 +0300

    Updates

---
+++ precache-manifest.2412da2d16a578bb505e57ae858f0ff5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddfa98121f109e6625d900a7e807ed9a",
+    "url": "/React_stars_searchbox/index.html"
+  },
+  {
+    "revision": "e36875678181ee9132a6",
+    "url": "/React_stars_searchbox/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "1fc7b8677561ecbb50e0",
+    "url": "/React_stars_searchbox/static/css/main.032fc2f2.chunk.css"
+  },
+  {
+    "revision": "e36875678181ee9132a6",
+    "url": "/React_stars_searchbox/static/js/2.f8c1f08a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_stars_searchbox/static/js/2.f8c1f08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc7b8677561ecbb50e0",
+    "url": "/React_stars_searchbox/static/js/main.d661db04.chunk.js"
+  },
+  {
+    "revision": "f00d08887a11d1e8adac",
+    "url": "/React_stars_searchbox/static/js/runtime-main.7cdbcda5.js"
+  },
+  {
+    "revision": "8a4e92abd503ea64d125956d5d777a3d",
+    "url": "/React_stars_searchbox/static/media/Lovely Kei.8a4e92ab.woff"
+  }
+]);
